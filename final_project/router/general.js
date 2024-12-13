601cb0fc60ffa596884c38f0fd8af7e2ccf2d664
rachit4018 <pandyarachit6@gmail.com>
2024-12-12 23:39:16 -0500

    implemented the books and isbn api endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,25 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if(isbn){
+        const bks = books[isbn]
+        if(bks){
+            res.send(JSON.stringify(bks,null,4));
+        }
+        else{
+            res.send("Sorry, no book found");
+        }
+    }
+    else{
+        res.send("Please, enter the isbn");
+    }
  });
   
 // Get book details based on author
