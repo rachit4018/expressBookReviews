af3d8f3318e1ae3973b664995daafcd2434ed6df
rachit4018 <pandyarachit6@gmail.com>
2024-12-15 21:27:04 -0500

    author api developed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,7 +37,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+
+  // If no books are found, return an error response
+  if (filteredBooks.length === 0) {
+    return res.status(404).json({ message: "No books found for the specified author." });
+  }
+
+  // Return the matching books
+  return res.status(200).json(filteredBooks);
+    
 });
 
 // Get all books based on title
